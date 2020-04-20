5242f9459feceb7e40ef843b24d314ef9a4aec0d
januszpolak <janusz.polak@interia.pl>
2020-04-20 15:11:17 +0200

    update room section

--- form.php
+++ form.php
@@ -6,4 +6,18 @@ $surname = ($_POST['surname']);
 $email = ($_POST['email']); //pobieranie danych o e-mailu nadawcy
 $message = ($_POST['message']); //pobieranie wiadomości
 
+//w $header tworzymy nagłówek e-mail (dane o kodowaniu, gdzie ma być e-mail wysłany itp.
+$header = "From: $email \nContent-Type:".
+            ' text/plain;charset="UTF-8"'.
+            "\nContent-Transfer-Encoding: 8bit";
+    if (mail($do, $temat, $wiadomosc, $header))
+        {
+        echo ('E-mail został wyslany!'); //jeżeli e-mail się wyśle, pokazywanie tego komunikatu
+        }
+    else
+    {
+        echo ('Blad podczas wysylania e-maila!');
+        }
+ 
+
 ?>
