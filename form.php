6ff7d624f82793a7734525f7bf73ea015fb97fd6
Janusz Polak <janusz.polak@interia.pl>
2020-05-08 09:47:47 +0200

    change social icons size

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
  
 $do = 'janojp@gmail.com'; //tutaj wpisujesz swój adres e-mail
 $name = ($_POST['name']);
-$surname = ($_POST['surname']);
+$surname = ($_POST['surname']);	
 $email = ($_POST['email']); //pobieranie danych o e-mailu nadawcy
 $message = ($_POST['message']); //pobieranie wiadomości
 
@@ -10,7 +10,7 @@ $message = ($_POST['message']); //pobieranie wiadomości
 $header = "From: $email \nContent-Type:".
             ' text/plain;charset="UTF-8"'.
             "\nContent-Transfer-Encoding: 8bit";
-    if (mail($do, $temat, $wiadomosc, $header))
+    if (mail($do, $message))
         {
         echo ('E-mail został wyslany!'); //jeżeli e-mail się wyśle, pokazywanie tego komunikatu
         }
